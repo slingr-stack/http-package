205c63f0c628caa96e2ab2751d91bd64d79e5d10
pasaperez <aperez@slingr.io>
2023-09-26 18:33:46 -0300

    fix

--- flowSteps/httpCall/step.js
+++ flowSteps/httpCall/step.js
@@ -31,9 +31,9 @@ step.httpCall = function (inputs) {
 		headers: inputs.headers || [],
 		params: inputs.params || [],
 		body: inputs.body || {},
-		followRedirects : inputs.advancedSettings.followRedirects || false,
-		connectionTimeout: inputs.advancedSettings.connectionTimeout || 5000,
-		readTimeout: inputs.advancedSettings.readTimeout || 60000
+		followRedirects : inputs.followRedirects || false,
+		connectionTimeout: inputs.connectionTimeout || 5000,
+		readTimeout: inputs.readTimeout || 60000
 	};
 
 	inputsLogic.headers = isObject(inputsLogic.headers) ? inputsLogic.headers : stringToObject(inputsLogic.headers);
